a644e18235241acbbc7b835d816f3687ca9e58a8
Kiyuzu <hikkiprc@gmail.com>
2023-10-05 00:38:32 +0800

    Added enums for future use

--- Assets/Scripts/Utilities/Consts.cs
+++ Assets/Scripts/Utilities/Consts.cs
@@ -17,4 +17,69 @@ namespace Game
 	{
 
 	}
+
+	/// <summary>
+	/// 角色类型
+	/// </summary>
+	public enum CharacterType
+	{
+		Library = 0,
+		Player1 = 1,
+		Player2 = 2,
+		Player3 = 3,
+		Desk
+	}
+
+	/// <summary>
+	/// 花色
+	/// </summary>
+	public enum Colours
+	{
+		None,
+		Club,//梅花
+		Heart,//红桃
+		Spade,//黑桃
+		Diamond//方片
+	}
+
+	/// <summary>
+	/// 卡牌权重
+	/// </summary>
+	public enum Weight
+	{
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+		Ace,
+		Two,
+		BlackJoker,
+		RedJoker
+	}
+
+	/// <summary>
+	/// 出牌类型
+	/// </summary>
+	public enum CardType
+	{
+		None,
+		Single,//单张
+		Double,//对
+		Straight,//顺子
+		DoubleStraight,//姐妹花
+		TripleStraight,//三张的顺子
+		Three,//三张
+		ThreeOne,//三带一
+		ThreeTwo,//三带二
+		Plane,//飞机
+		Bomb,//炸弹
+		JokerBomb,
+	}
 }
