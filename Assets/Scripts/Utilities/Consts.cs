d0a57a85e60c737551fbc7cc4e069624e218fba2
Kiyuzu <hikkiprc@gmail.com>
2023-10-04 22:38:42 +0800

    Added Scripts/Utilities for future use

---
+++ Assets/Scripts/Utilities/Consts.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Game
+{
+	public class Consts
+	{
+		/// <summary>
+		/// 游戏数据的路径
+		/// </summary>
+		public static readonly string DataPath = Application.persistentDataPath+@"\data.xml";
+	}
+
+	/// <summary>
+	/// UI 面板的类型
+	/// </summary>
+	public enum PanelType
+	{
+
+	}
+}
