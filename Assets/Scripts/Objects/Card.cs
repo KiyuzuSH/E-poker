740331205f2d7bd9fbe68dbc67da315b6558d772
Kiyuzu <hikkiprc@gmail.com>
2023-10-05 18:14:51 +0800

    Complete basic logic and added all materials

---
+++ Assets/Scripts/Objects/Card.cs
@@ -0,0 +1,53 @@
+namespace Game
+{
+	/// <summary>
+	/// 卡牌
+	/// </summary>
+	public class Card
+	{
+		private string cardName;
+		private Colours cardColour;
+		private Weight cardWeight;
+		private CharacterType belongTo;
+
+		/// <summary>
+		/// 卡牌名字
+		/// </summary>
+		public string CardName
+		{
+			get { return cardName; }
+		}
+
+		/// <summary>
+		/// 卡牌花色
+		/// </summary>
+		public Colours CardColour
+		{
+			get { return cardColour; }
+		}
+
+		/// <summary>
+		/// 卡牌权值
+		/// </summary>
+		public Weight CardWeight
+		{
+			get { return cardWeight; }
+		}
+
+		/// <summary>
+		/// 卡牌由谁持有
+		/// </summary>
+		public CharacterType BelongTo
+		{
+			get { return belongTo; }
+		}
+
+		public Card(string name,Colours colour, Weight weight,CharacterType belongTo)
+		{
+			this.cardName = name;
+			this.cardColour = colour;
+			this.cardWeight = weight;
+			this.belongTo = belongTo;
+		}
+	}
+}
